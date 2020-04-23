a91e731a24da569820e6700acdba883ee00d86b9
F. Javier del Castillo <javicastillo92@gmail.com>
2020-04-23 20:53:18 +0200

    app start script

---
+++ run.sh
@@ -0,0 +1,10 @@
+# to run this script first -> chmod u+x run.sh
+echo '----------------------'
+echo '+ Activate VIRTUAL ENV'
+source venv/bin/activate
+
+echo '+ Export PYTHON PATH'
+export PYTHONPATH="$PWD"
+
+echo '+ Run APP\n'
+python src/app.py
